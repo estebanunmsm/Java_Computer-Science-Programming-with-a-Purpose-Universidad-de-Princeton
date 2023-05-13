3f24fc577195c5ba8fd6638e7631550bae8c0aca
estebanunmsm <joseesteban.ocana@unmsm.edu.pe>
2023-05-12 19:53:23 -0500

    Sem 1

---
+++ hello/HelloGoodbye.java
@@ -0,0 +1,9 @@
+public class HelloGoodbye {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.print(args[0]);
+        System.out.print(" and ");
+        System.out.println(args[1]+ ".");
+        System.out.print("Goodbye " + args[1] + " and " + args[0] + ".");
+    }
+}
